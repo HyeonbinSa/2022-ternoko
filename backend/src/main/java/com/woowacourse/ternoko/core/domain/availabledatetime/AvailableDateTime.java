fdf9214821c76fe02dd2591aa1d658b535d42749
her0807 <her0807@naver.com>
2022-10-18 16:03:21 +0900

    test: strategy Auto 로 변경

--- backend/src/main/java/com/woowacourse/ternoko/core/domain/availabledatetime/AvailableDateTime.java
+++ backend/src/main/java/com/woowacourse/ternoko/core/domain/availabledatetime/AvailableDateTime.java
@@ -26,7 +26,7 @@ import lombok.NoArgsConstructor;
 public class AvailableDateTime {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(name = "coach_id", nullable = false)
